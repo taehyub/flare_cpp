05667d96fea4daff243b773cedc8da8625b7f26e
Taehyub Kim <taehyub.kim@samsung.com>
2020-10-14 20:22:45 +0900

    Implement TvgColorFill, TvgColorStroke Opacity Option

--- flare_skia/src/skr_color_fill.cpp
+++ flare_skia/src/skr_color_fill.cpp
@@ -21,7 +21,7 @@ void TvgColorFill::updatePaint()
     this->m_color[0] = m_Color[0];
     this->m_color[1] = m_Color[1];
     this->m_color[2] = m_Color[2];
-    this->m_color[3] = m_Color[3];
+    this->m_color[3] = m_Color[3] * m_RenderOpacity;
 
     onPaintUpdated();
 }
--- flare_skia/src/skr_color_stroke.cpp
+++ flare_skia/src/skr_color_stroke.cpp
@@ -24,7 +24,7 @@ void TvgColorStroke::updatePaint()
     this->m_color[0] = m_Color[0];
     this->m_color[1] = m_Color[1];
     this->m_color[2] = m_Color[2];
-    this->m_color[3] = m_Color[3];
+    this->m_color[3] = m_Color[3] * m_RenderOpacity;
 
     onPaintUpdated();
 }
